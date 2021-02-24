cfea4aeaf563e4952083bae6549f7f7b02082781
Maicon Lourenço <maiconloure@gmail.com>
2021-02-24 13:02:27 -0300

    added survey, new migrations, tests with jest

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,16 +1,16 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from "../models/User";
+import { getCustomRepository } from 'typeorm';
+import { UserRepository } from '../repositories/UserRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
     const { name, email} = request.body;
 
-    const usersRepository = getRepository(User);
+    const usersRepository = getCustomRepository(UserRepository);
 
     const userAlreadyExists = await usersRepository.findOne({
       email
-    })
+    });
 
     if (userAlreadyExists) {
       return response.status(400).json({
@@ -24,7 +24,7 @@ class UserController {
 
     await usersRepository.save(user);
 
-    return response.status(200).json({
+    return response.status(201).json({
       message: "User created successfully!"
     });
   }
---
+++ src/models/Survey.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+@Entity("surveys")
+class Survey {
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { Survey };
