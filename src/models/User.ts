81747b86dcaa4d8fe31520fe160f160d8067535f
Maicon Lourenço <maiconloure@gmail.com>
2021-02-24 10:09:06 -0300

    initial commit

---
+++ src/models/User.ts
@@ -0,0 +1,26 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+@Entity("users")
+class User {
+  
+  @PrimaryColumn()
+  readonly id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { User };
